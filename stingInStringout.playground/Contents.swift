58e9fcc7bc724634b364b3d312ebaceff8179d4c
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-23 20:59:11 +0700

    Variabels and string dan overview first app

---
+++ stingInStringout.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str: String = "Hello, playground"
+
+var firstName = "Faza"
+
+var lastName = "Fahamsyah"
+
+var age = 27
+
+var FullName = firstName + " " + lastName
+
+var fullname2 = "\(firstName) \(lastName) is \(age)"
+
+FullName.append(" III")
+
+var bookTitle = "revenge of the crab cakes"
+
+bookTitle = bookTitle.capitalized
+
+var chatRoomAnnoyingCapsGuy = "PLEASE HELP ME NOW! HERE IS MY 100 LINES OF CODE"
+
+var lowerCassedChat = chatRoomAnnoyingCapsGuy.lowercased()
---
+++ variabels.playground/Contents.swift
@@ -0,0 +1,19 @@
+ //: Playground - noun: a place where people can play
+
+import UIKit
+
+var Message = "Hello, playground"
+ 
+ // Unary, Binary, ternary
+ 
+ var iAmCool = true
+ 
+ iAmCool = !iAmCool
+ 
+ var feelGoodAboutMySelf = true
+ 
+ feelGoodAboutMySelf = iAmCool ? true : false
+ 
+ var bankAccountBallance = 100
+ 
+ var cashRegisterMessage = bankAccountBallance >= 50 ? "You just bought the item" : "You are broke as a joke"
