f757d929a403099ef024474f45b8e99b79412d4d
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-23 21:07:02 +0700

    full string

--- stingInStringout.playground/Contents.swift
+++ stingInStringout.playground/Contents.swift
@@ -23,3 +23,11 @@ bookTitle = bookTitle.capitalized
 var chatRoomAnnoyingCapsGuy = "PLEASE HELP ME NOW! HERE IS MY 100 LINES OF CODE"
 
 var lowerCassedChat = chatRoomAnnoyingCapsGuy.lowercased()
+
+
+var sentence = "What the fetch?! Heck that is crazy!"
+
+if sentence.contains("fetch") || sentence.contains("Heck"){
+    sentence.replacingOccurrences(of: "fetch", with: "Tuna")
+    sentence.replacingOccurrences(of: "Heck", with: "Play")
+}
