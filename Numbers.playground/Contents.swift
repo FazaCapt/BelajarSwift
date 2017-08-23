ffca2ce554ba5478dcc4a2aa1c43b6bde56c67a3
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-24 00:11:59 +0700

    Numbers

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,76 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Numbers
+// Type inference
+
+var age = 27
+
+//Explicity declered type
+var weigth: Int = 93
+
+// var someNum : UInt8 = 1723218371283128312
+
+var someNum : Double = 1723218371283128312
+
+
+var milesRan = 56.45
+
+var pi: Float = 3.14
+
+// pi = milesRan
+
+// Aritmatic operator
+
+var area = 15 * 23
+
+var sum = 34 + 45
+
+var diff = 10 - 3
+
+var div  = 12 / 3
+
+var div1 = 13 / 5
+
+
+var remainder = 13 % 5
+
+var result = "The result of 13/5 is \(div1) with a remainder of \(remainder) "
+
+var randomNumber = 12
+
+if randomNumber % 2 == 0 {
+    print("This is an even number")
+} else {
+    print("This is an odd number")
+}
+
+var result2 = 15 * ((5+7)/3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
