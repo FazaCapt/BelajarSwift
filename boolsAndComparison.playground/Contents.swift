b7968c65b6ccbaed91207b204659f3df180679d8
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-24 08:33:29 +0700

    Bool and condition and comparison

---
+++ boolsAndComparison.playground/Contents.swift
@@ -0,0 +1,84 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var bestTeacher: Bool  = true
+
+bestTeacher = false
+
+if true == false || true == true {
+    print("Fish")
+}
+
+var DownloadFinish: Bool = true
+
+// ...
+//....
+
+DownloadFinish = true
+
+// load UI and other app features
+
+if 2 == 2 {
+    print("Should not see this")
+}
+
+// Load  UI and other app features 
+
+// Equal to     :   ==
+// Not Equal to :   !=
+// Greater than :   >
+// Greater than or equal  : >=
+// Less than or equal to: <=
+// Less than: <
+
+var bankBalance = 400
+var itemToBuy = 400
+
+if bankBalance >= itemToBuy{
+    print("purchased item")
+}
+if itemToBuy > bankBalance {
+    print("You need mo' money foo")
+}
+
+if itemToBuy == bankBalance {
+    print("Hey Buddy, Your ballance is now 0")
+}
+
+var bookTitle1 = "Harry bloter and the moppet of mire"
+var bookTitle2 = "Harry bloter and the moppet of mire"
+
+if bookTitle1 != bookTitle2 {
+    print("Need to fix spelling before printing")
+    
+} else if bookTitle2.characters.count > 10 {
+    print("find a new title for the book")
+}
+else {
+    print("Books looks great send to printer")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
