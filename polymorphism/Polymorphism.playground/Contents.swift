93c64b366218767b04f3adae316c8f363fe2c366
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-24 15:18:02 +0700

    Polymorphism

---
+++ polymorphism/Polymorphism.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+class Shape {
+    var area: Double?
+    
+    func calculateArea(valA: Double, valB: Double){
+        
+    }
+}
+
+class Triangle: Shape {
+    override func calculateArea(valA: Double, valB: Double) {
+        area = (valA * valB) / 2
+    }
+}
+
+class Rectangle: Shape {
+    override func calculateArea(valA: Double, valB: Double) {
+        area = valA * valB
+    }
+}
+
+
+//class Rectangle {
+//    var area: Double?
+//    
+//    func calculateArea(length: Double, width: Double){
+//        area = length * width
+//    }
+//}
+//
+//class Triangle {
+//    var area: Double?
+//    
+//    func calculateArea(base: Double, height: Double){
+//        area = (base * height) / 2
+//    }
+//}
